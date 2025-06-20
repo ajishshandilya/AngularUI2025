f4f27a188f49dfa6b1621917b43f3581ccf7f4c8
Ajish Kumar <ajish.kumar@mobileprogramming.com>
2025-06-20 17:01:04 +0530

    created Articture

---
+++ ng-topic-wise/src/app/app-home/topics/decorators/decorators.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-decorators',
+  imports: [],
+  templateUrl: './decorators.html',
+  styleUrl: './decorators.scss'
+})
+export class Decorators {
+
+}
--- ng-topic-wise/src/app/app.routes.ts
+++ ng-topic-wise/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ApiCall } from './app-home/topics/api-call/api-call';
+import { AppHome } from './app-home/app-home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'api-call', component: ApiCall },
+  { path: 'home', component: AppHome },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' }
+];
